989ad6062810bece30f2ece9a887605552437602
dolka-programmist <74834904+dolka-programmist@users.noreply.github.com>
2021-01-16 20:32:05 +0300

    Update and rename README.md to style.css

---
+++ style.css
@@ -0,0 +1,20 @@
+# style.css
+<html>
+    <head>
+        <title>Магазин фейверков</title>
+        <link rel="stylesheet"
+        href="style.css">
+    </head>
+<h1 id="price">Наши фейерверки</h1>
+            <p>
+                <p><img src="https://mars.algoritmika.org/uploads/2020/12/%D1%85%D0%BB%D0%BE%D0%BF%D1%83%D1%88%D0%BA%D0%B0_0_1607673818.svg" width="70px"/><br/>Хлопушки</p>
+                <p><img src="https://mars.algoritmika.org/uploads/2020/12/%D0%BF%D0%B5%D1%82%D0%B0%D1%80%D0%B4%D0%B0_0_1607673817.svg" width="70px"/><br/>Петарды</p>
+                <p><img src="https://mars.algoritmika.org/uploads/2020/12/%D1%84%D0%BE%D0%BD%D1%82%D0%B0%D0%BD_0_1607673817.svg" width="70px"/><br/>Фонтаны</p>
+                <p><img src="https://mars.algoritmika.org/uploads/2020/12/%D1%88%D0%B0%D1%80_0_1607673818.svg" width="70px"/></p><br/>Фестивальные шары</p>
+                <h2 id="discount">Сезонное предложение</h2>
+            <p>С 5 декабря по 5 марта действует предложение: скидка на всю продукцию 40%.</p>
+            <p>
+                <i>Спешите купить праздник выгодно!</i>
+            </p>
+            <img src="https://mars.algoritmika.org/uploads/2020/12/%D1%81%D0%B5%D0%B7%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5%20%D1%81%D0%BA%D0%B8%D0%B4%D0%BA%D0%B8_0_1607673818.jpg" width="850px"/>
+</html>
